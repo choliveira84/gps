0c66dc38583618b187de96b738ebdb41447d4288
Carlos Henrique de Oliveira <choliveira84@gmail.com>
2017-09-18 22:41:58 -0300

    criação da estrutura do projeto e classe Ponto.

---
+++ src/main/java/br/com/choliveira/entidade/Ponto.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package br.com.choliveira.entidade;
+
+/**
+ * @author carlo
+ * @since 18/09/2017
+ */
+public class Ponto {
+	private int x;
+	private int y;
+	private int z;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	/**
+	 * A posição do submarino é representado pela notação (0, 0, 0, NORTE)
+	 */
+	public Ponto() {
+		this.x = 0;
+		this.y = 0;
+		this.z = 0;
+	}
+
+	@Override
+	public String toString() {
+		return getX() + " " + getY() + " " + getZ();
+	}
+}
